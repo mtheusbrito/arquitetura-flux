ce848df47df423b1fdecf4687f2e82cf1daa0028
matheus <matheusbritodasilva@gmail.com>
2019-10-22 14:45:06 -0300

    Estilização do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th>-</th>
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe2.jpg?ims=326x"
+                                alt="Tenis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis muito massa</strong>
+                            <strong>R$ 129, 90</strong>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$ 258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$ 1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
