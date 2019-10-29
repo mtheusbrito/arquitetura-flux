1e683d735aba133e00378119bf6fd10512759e66
matheus <matheusbritodasilva@gmail.com>
2019-10-29 16:03:39 -0300

    Calculando totais

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,10 +6,11 @@ import {
     MdAddCircleOutline,
     MdDelete,
 } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 import * as CartActions from '../../store/modules/cart/actions';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, removeFromCart, updateAmount, total }) {
     function increment(product) {
         updateAmount(product.id, product.amount + 1);
     }
@@ -66,7 +67,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                                 </div>
                             </td>
                             <td>
-                                <strong>R$ 258,80</strong>
+                                <strong>{product.subtotal}</strong>
                             </td>
                             <td>
                                 <button
@@ -85,14 +86,22 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 <button type="button">Finalizar pedido</button>
                 <Total>
                     <span>TOTAL</span>
-                    <strong>R$ 1920,28</strong>
+                    <strong>{total}</strong>
                 </Total>
             </footer>
         </Container>
     );
 }
 const mapStateToProps = state => ({
-    cart: state.cart,
+    cart: state.cart.map(product => ({
+        ...product,
+        subtotal: formatPrice(product.price * product.amount),
+    })),
+    total: formatPrice(
+        state.cart.reduce((total, product) => {
+            return total + product.price * product.amount;
+        }, 0)
+    ),
 });
 
 const mapDispatchToProps = dispatch =>
