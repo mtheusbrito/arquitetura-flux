6e2d7a92aad4fa91ee77528a983e4fbf309b4a61
matheus <matheusbritodasilva@gmail.com>
2019-10-30 16:56:37 -0300

    reactotron+saga

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,21 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMidleware = createSagaMiddleware();
+const sagaMonitor =
+    process.env.NODE_ENV === 'development'
+        ? console.tron.createSagaMonitor()
+        : null;
+const sagaMiddleware = createSagaMiddleware({
+    sagaMonitor,
+});
 
-const enhacer =
+const enhancer =
     process.env.NODE_ENV === 'development'
         ? compose(
               console.tron.createEnhancer(),
-              applyMiddleware(sagaMidleware)
+              applyMiddleware(sagaMiddleware)
           )
-        : applyMiddleware(sagaMidleware);
-const store = createStore(rootReducer, enhacer);
-sagaMidleware.run(rootSaga);
+        : applyMiddleware(sagaMiddleware);
+const store = createStore(rootReducer, enhancer);
+sagaMiddleware.run(rootSaga);
 export default store;
